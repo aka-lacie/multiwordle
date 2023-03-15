059d5399ea01b36bdbd50816cfb4f8e1b2b61a73
Freya Zhu <freyazhu@Freyas-MacBook-Air.local>
2023-03-15 14:42:31 -0700

    idk

--- components/Body.tsx
+++ components/Body.tsx
@@ -8,38 +8,38 @@ import { Alert } from 'react-bootstrap'
 
 let socket : SocketIOClientSocket;
 
-export default function Body() {
-  useEffect(() => socketInitializer(), [])
+export default function Body({ socket }) {
+  useEffect(() => { 
+    listenerInitializer()
+    return () => {
+      socket.removeAllListeners(['action-rejected', 'is-host', 'start-game'])
+    }
+  }, [])
 
-  const socketInitializer = () => {
-    fetch('/api/socket').then(() => {
-      socket = io()
-  
-      socket.on("connect", () => {
-        console.log(`connected ${socket.id}`);
-      });
-  
+  const listenerInitializer = () => {
       socket.on("action-rejected", (reason : string) => {
         setShowAlert({ show: true, msg: reason })
       });
 
+      console.log("starting listening for is-host")
       socket.on("is-host", (room: string) => {
-        setHostState({ isHost: true, room });
+        console.log("you are the host of this room")
+        setHostState(true);
         setShowAlert({ show: true, msg: "You are the host of this room." })
       });
+      console.log("finished listening for is-host")
 
       socket.on("start-game", () => {
         console.log("starting game");
         setGameState('Game');
       });
-    });
   }
   
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
-  const [hostState, setHostState] = useState({ isHost: false, room: '' });
+  const [hostState, setHostState] = useState(false);
 
-  const [showAlert, setShowAlert] = useState({show: false, msg: ''});
+  const [showAlert, setShowAlert] = useState({ show: false, msg: '' });
   const [gameState, setGameState] = useState('Login');
 
   
@@ -66,7 +66,7 @@ export default function Body() {
     case 'WaitingRoom':
       return (<>
         { showAlert.show && <Alert variant="primary" onClose={() => setShowAlert({ show: false, msg: '' })} dismissible>{showAlert.msg}</Alert> }
-        <WaitingRoom username={name} room={room} isHost={hostState.isHost} onStartGame={handleStartGame}/>
+        <WaitingRoom username={name} room={room} isHost={hostState} onStartGame={handleStartGame}/>
       </>)
     case 'Game':
       return (<>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
 import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from '../components/Login'
-import WaitingRoom from '../components/WaitingRoom'
-import io from 'socket.io-client'
-import { Socket as SocketIOClientSocket } from 'socket.io-client';
+import io, { Socket as SocketIOClientSocket } from 'socket.io-client';
 import { Alert } from 'react-bootstrap'
 import Body from '@/components/Body'
 
 export default function Home() {
+  const [socket, setSocket] = useState<SocketIOClientSocket | null>(null);
+
+  useEffect(() => {
+    socketInitializer();
+    return () => {
+      if (socket) socket.disconnect();
+    };
+  }, [])
+
+  const socketInitializer = () => {
+    fetch('/api/socket').then(() => {
+      let sock = io()
+
+      sock.on("connect", () => {
+        console.log(`connected ${sock.id}`);
+        setSocket(sock);
+      });
+    });
+  }
 
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>'Round The Wordle</h1>
       <hr/>
-      <Body />
+      <div>Your ID: { socket && socket.id}</div>
+      { socket && <Body socket={socket}/> }
     </>
   )
 }
